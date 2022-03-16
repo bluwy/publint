ee7b8300cb9cfdd288c44c85116c4e73517736aa
bluwy <bjornlu.dev@gmail.com>
2022-03-16 12:32:34 +0800

    Add vite-plugin-svelte

---
+++ site/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+export default defineConfig({
+  plugins: [
+    svelte({
+      experimental: {
+        prebundleSvelteLibraries: true,
+        useVitePreprocess: true
+      }
+    })
+  ]
+})
