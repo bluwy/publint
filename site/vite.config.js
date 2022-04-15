9e365c13998e50294aeb2b43724b906c63a56acf
bluwy <bjornlu.dev@gmail.com>
2022-04-15 17:07:03 +0800

    Handle dot in path refresh

--- site/vite.config.js
+++ site/vite.config.js
@@ -4,6 +4,7 @@ import unocss from '@unocss/vite'
 
 export default defineConfig({
   plugins: [
+    spaFallbackWithDot(),
     unocss(),
     svelte({
       experimental: {
@@ -13,3 +14,23 @@ export default defineConfig({
     })
   ]
 })
+
+/**
+ * Vite doesn't handle fallback html with dot (.), see https://github.com/vitejs/vite/issues/2415
+ * @returns {import('vite').Plugin}
+ */
+function spaFallbackWithDot() {
+  return {
+    name: 'spa-fallback-with-dot',
+    configureServer(server) {
+      return () => {
+        server.middlewares.use(function customSpaFallback(req, res, next) {
+          if (req.url.includes('.')) {
+            req.url = '/index.html'
+          }
+          next()
+        })
+      }
+    }
+  }
+}
