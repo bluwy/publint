7969f565db2c5c233a8b9f77a61bbb052fad7d71
bluwy <bjornlu.dev@gmail.com>
2022-04-15 18:20:39 +0800

    Simplify home page

--- site/src/pages/Home.svelte
+++ site/src/pages/Home.svelte
@@ -2,7 +2,7 @@
   import NpmSearchInput from '../components/NpmSearchInput.svelte'
 </script>
 
-<main class="flex flex-col items-center h-full mt-5">
+<main class="flex flex-col justify-center items-center h-screen mt-5s">
   <h1 class="mb-0">publint</h1>
   <p>Lint before you publish!</p>
   <section class="flex flex-col justify-center items-center w-full">
