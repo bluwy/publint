37591fade4ad1463a938aaf2a1e20446f330c0fe
bluwy <bjornlu.dev@gmail.com>
2022-11-05 15:07:14 +0800

    Fallback fetch packument for latest check
    fix #6

--- site/src/pages/Package.svelte
+++ site/src/pages/Package.svelte
@@ -36,14 +36,20 @@
   $: if (npmPkgVersion) {
     versionFetched = true
   } else {
-    fetch(
-      // prettier-ignore
-      `${import.meta.env.VITE_NPM_REGISTRY}/-/v1/search?text=${encodeURIComponent(npmPkgName)}&size=1&quality=0.0&popularity=1.0&maintenance=0.0`
-    )
-      .then((v) => v.ok && v.json())
-      .then((v) => {
-        if ((v = v.objects[0])) {
-          url.replace(`/${npmPkgName}@${v.package.version}`)
+    fetchPackageLatestVersionFast(npmPkgName)
+      .then((version) => {
+        if (version) {
+          return version
+        } else {
+          throw new Error()
+        }
+      })
+      .catch(() => {
+        return fetchPackageLatestVersionSlow(npmPkgName)
+      })
+      .then((version) => {
+        if (version) {
+          url.replace(`/${npmPkgName}@${version}`)
         }
       })
       .finally(() => {
@@ -122,6 +128,36 @@
       return { logo: githubLogo, url }
     } else if (url.includes('gitlab.com')) {
       return { logo: gitlabLogo, url }
+    }
+  }
+
+  async function fetchPackageLatestVersionFast(pkgName) {
+    // use search result for smaller payload
+    const result = await fetch(
+      // prettier-ignore
+      `${import.meta.env.VITE_NPM_REGISTRY}/-/v1/search?text=${encodeURIComponent(pkgName)}&size=1&quality=0.0&popularity=1.0&maintenance=0.0`
+    )
+    if (result.ok) {
+      const json = await result.json()
+      return json.objects[0]?.package.version
+    }
+  }
+
+  async function fetchPackageLatestVersionSlow(pkgName) {
+    // use packument for unsearchable packages, but larger payload
+    const result = await fetch(
+      // prettier-ignore
+      `${import.meta.env.VITE_NPM_REGISTRY}/${encodeURIComponent(pkgName)}`,
+      {
+        headers: {
+          Accept:
+            'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*'
+        }
+      }
+    )
+    if (result.ok) {
+      const json = await result.json()
+      return json['dist-tags']?.latest
     }
   }
 </script>
