57e7a13a98bacccd6758daefea95ce5c798384ac
bluwy <bjornlu.dev@gmail.com>
2023-03-21 02:09:09 +0800

    Link to are the types wrong

--- site/src/pages/Home.svelte
+++ site/src/pages/Home.svelte
@@ -18,5 +18,15 @@
       <strong class="inline-block mx-1">-</strong>
       <a class="anchor-link" href="/rules.html">Rules</a>
     </p>
+    <div class="mt-12 -mb-12 text-center">
+      <h2 class="text-sm font-500 opacity-70">Other handy tools:</h2>
+      <ul class="list-none opacity-60 p-0 m-0">
+        <li>
+          <a class="anchor-link" href="https://arethetypeswrong.github.io">
+            Are the types wrong?
+          </a>
+        </li>
+      </ul>
+    </div>
   </section>
 </main>
