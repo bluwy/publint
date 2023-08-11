51352d54725c2dee981b8f6e624cf25884032595
bluwy <bjornlu.dev@gmail.com>
2023-08-11 19:46:53 +0800

    Setup analysis CI

--- analysis/gist.sh
+++ analysis/gist.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+DEFAULT_ANALYSIS_GIST=git@gist.github.com:64b0c283d8f0f3f8a8f4eea03c75a3b8.git
+ANALYSIS_GIST=${ANALYSIS_GIST:-$DEFAULT_ANALYSIS_GIST}
+
 echo "Updating gist..."
 mkdir -p gist
 cp cache/_results.json gist/publint_analysis.json
 cd gist
 git init
-git remote add origin git@gist.github.com:64b0c283d8f0f3f8a8f4eea03c75a3b8.git
+git remote add origin "$ANALYSIS_GIST"
 git add publint_analysis.json
 git commit -m "init"
 git push -f origin master
