5346c95de77083bc69eea84d41365294abd5912f
bluwy <bjornlu.dev@gmail.com>
2024-12-30 11:08:25 +0800

    [skip ci] Update docs

--- packages/packlist/index.d.ts
+++ packages/packlist/index.d.ts
@@ -6,10 +6,15 @@ export interface Options {
    */
   packageManager?: 'npm' | 'yarn' | 'pnpm' | 'bun'
   /**
-   * How to pack the given directory to figure out the list of files:
-   * - `'json'`: Uses `pack --json` to get the list of files (works with all package manager except pnpm <9.14.1 and bun).
-   * - `'pack'`: Uses `pack --pack-destination` to get the list of files (works with all package managers).
+   * How to pack the given directory to get the list of files:
+   * - `'json'`: Uses `<pm> pack --json` (works with all package manager except pnpm <9.14.1 and bun).
+   * - `'pack'`: Uses `<pm> pack --pack-destination` (works with all package managers).
    * - `'json-and-pack'`: Tries to use `'json'` first, and if it fails, falls back to `'pack'`.
+   *
+   * NOTE: Theoretically, `'json'` should be faster than `'pack'`, but all
+   * package managers seem to only support it as an alternate stdout format
+   * and there's no significant speed difference in practice. However, `'json'`
+   * performs less fs operations internally so should still be slightly faster.
    *
    * @default 'json-and-pack'
    */
